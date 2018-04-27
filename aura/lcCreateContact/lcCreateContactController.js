eddb2a1cd18a3d1676873fcc7db78e6011256237
rlou0502@yahoo.com <rlou0502@yahoo.com>
2018-04-27 16:00:28 -0500

    'changes from sfdc'

--- aura/lcCreateContact/lcCreateContactController.js
+++ aura/lcCreateContact/lcCreateContactController.js
@@ -1,16 +1,23 @@
 ({
     doInit : function (component, event, helper) {
         var createRecordEvent = $A.get("e.force:createRecord");
-        var recordId = component.get("v.recordId");
-        var recordTypeId = component.get("v.recordTypeId");
-        alert('recordTypeId: '+recordTypeId);
         var strParentId;
+        var strRecordTypeId;
         var currentURL = window.location.toString();
         var iAccId = currentURL.indexOf('=accid');
+        var iRecordTypeId = currentURL.indexOf('recordTypeId=');
         if(iAccId>=0){          // this is id for account (you can get first 3 number from id of you parent record) 
     		strParentId = currentURL.substring(iAccId+9,iAccId+24); // find parent id from string url  
 		}
-		alert(strParentId);		
+        alert('strParentId: '+strParentId);	
+        alert('component.get("v.account.Id");: '+component.get("v.account.Id"));
+        alert('component.get("v.recordId");: '+component.get("v.recordId"));
+        alert('component.get("v.simpleFields.BillingCity");: '+component.get("v.simpleFields.BillingCity"));
+
+        if(iRecordTypeId>=0){          // this is record type id for the new record
+    		strRecordTypeId = currentURL.substring(iRecordTypeId+13,iRecordTypeId+28); // find record type id from string url  
+		}
+        alert('strRecordTypeId: '+strRecordTypeId);		
         
         createRecordEvent.setParams({
             "entityApiName": "Contact",
