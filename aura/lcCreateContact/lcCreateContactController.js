2745eb061d9f15845fd89fd36bb08495b09136dd
rlou0502@yahoo.com <rlou0502@yahoo.com>
2018-04-27 22:04:16 -0500

    'changes from sfdc'

--- aura/lcCreateContact/lcCreateContactController.js
+++ aura/lcCreateContact/lcCreateContactController.js
@@ -9,22 +9,22 @@
         if(iAccId>=0){          // this is id for account (you can get first 3 number from id of you parent record) 
     		strParentId = currentURL.substring(iAccId+9,iAccId+24); // find parent id from string url  
 		}
-        alert('strParentId: '+strParentId);	
-        alert('component.get("v.account.Id");: '+component.get("v.account.Id"));
-        alert('component.get("v.recordId");: '+component.get("v.recordId"));
-        alert('component.get("v.simpleFields.BillingCity");: '+component.get("v.simpleFields.BillingCity"));
+//        alert('strParentId: '+strParentId);	
+//        alert('component.get("v.account.Id");: '+component.get("v.account.Id"));
+//        alert('component.get("v.recordId");: '+component.get("v.recordId"));
+//        alert('component.get("v.simpleFields.BillingCity");: '+component.get("v.simpleFields.BillingCity"));
 
         if(iRecordTypeId>=0){          // this is record type id for the new record
     		strRecordTypeId = currentURL.substring(iRecordTypeId+13,iRecordTypeId+28); // find record type id from string url  
 		}
-        alert('strRecordTypeId: '+strRecordTypeId);		
+//        alert('strRecordTypeId: '+strRecordTypeId);		
         
         createRecordEvent.setParams({
             "entityApiName": "Contact",
             "defaultFieldValues":{
                 'RecordTypeId':strRecordTypeId,
                 'AccountId':strParentId,
-	        }
+		        }
         });
         createRecordEvent.fire();
     }
