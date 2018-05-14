0288cb940c8be88353d281dea297e0903318ea03
rlou0502@yahoo.com <rlou0502@yahoo.com>
2018-05-14 13:00:25 -0500

    'changes from sfdc'

--- aura/ToroNT/ToroNTController.js
+++ aura/ToroNT/ToroNTController.js
@@ -1,6 +1,19 @@
 ({
-	initialize : function(component, event, helper) {
-		var quoteId = cmp.get('v.quoteId');
-		helper.initialize(quoteId);
+	initialize : function(cmp, event, helper) {
+		helper.initialize(cmp);
+	},
+    returnToPricing: function (cmp, event, helper) {
+    	console.log('@ToroSupportPlusController:returnToPricing');
+    	var quoteId = cmp.get("v.quoteId");
+    	document.location = '/apex/PricingProgramLgtnOut?Id=' + quoteId;
+	},
+	handleShowAddNewModal: function(cmp, event, helper) {
+		alert('handleShowAddNewModal placeholder');
+	},
+	handleSave: function(cmp, event, helper) {
+		alert('handleSave placeholder');
+	},
+	handleSaveAndClose: function (cmp, event, helper) {
+		alert('handleSaveAndClose placeholder');
 	}
 })
--- aura/ToroNT/ToroNTHelper.js
+++ aura/ToroNT/ToroNTHelper.js
@@ -1,12 +1,21 @@
 ({
-	initialize : function(quoteId) {
+	initialize: function (cmp) {
+		console.log('ToroNTHelper:initialize');
 		var action = cmp.get('c.retrieveNTData');
 		action.setParams({
-			quoteId: quoteId
+			quoteId: cmp.get('v.quoteId')
 		});
 		action.setCallback(
 			this, function(response) {
-				alert('retrievedNTData success');
+				if (cmp.isValid() && response.getState() === 'SUCCESS') {
+					var ntData = response.getReturnValue();
+					cmp.set('v.quote', ntData.quote);
+					cmp.set('v.quoteItems', ntData.qiWrappers);
+
+					console.log('ntData:');
+					console.log(ntData);
+
+				}
 			}
 		);
 
